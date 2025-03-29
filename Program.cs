9cf03949c27233e148911eaf02f6217eae429f0b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 22:00:30 -0300

    fim

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string fraseDigitada, fraseComDislalia;
+ 
+Console.Write("escreva uma frase: ");
+fraseDigitada = Console.ReadLine()! 
+.Replace("r", "l")
+    .Replace("R", "L");
+fraseComDislalia = fraseDigitada;
+   
+ Console.WriteLine("\n");
+Console.WriteLine(fraseComDislalia);
